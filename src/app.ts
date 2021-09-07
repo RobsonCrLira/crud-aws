2ce15f1501a72b7a3f0bce21f062df6ed65fe176
Robson Cristian de Lira <robso.crlira@gmail.com>
2021-09-06 21:40:30 -0300

    Estrutura inicial do projeto

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { NextFunction, Request, Response } from "express";
+
+import "express-async-errors";
+import cors from "cors";
+import { AppError } from "./errors/AppError";
+import { routes } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal Server Error : ${err.message}`,
+    });
+  }
+);
+export { app };
